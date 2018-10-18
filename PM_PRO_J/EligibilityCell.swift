e999d8182fd00daa5724aefe5236ccb056daded3
Travis Allen <travis.william.allen@gmail.com>
2018-10-17 21:54:06 -0400

    attempt to add inclusionexclusion table view

---
+++ PM_PRO_J/EligibilityCell.swift
@@ -0,0 +1,26 @@
+//
+//  EligibilityCell.swift
+//  PM_PRO_J
+//
+//  Created by Travis Allen on 10/17/18.
+//  Copyright © 2018 Travis Allen. All rights reserved.
+//
+
+import UIKit
+
+class EligibilityCell: UITableViewCell {
+
+    @IBOutlet weak var criterion: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+
+}
---
+++ PM_PRO_J/InclusionExclusionViewController.swift
@@ -0,0 +1,79 @@
+//
+//  InclusionExclusionViewController.swift
+//  PM_PRO_J
+//
+//  Created by Travis Allen on 10/17/18.
+//  Copyright © 2018 Travis Allen. All rights reserved.
+//
+
+import UIKit
+
+class InclusionExclusionViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var criteria: [String] = []
+    let cellReuseIdentifier = "cell"
+    let rightBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Continue", style: .plain, target: self, action: nil)
+//        barButtonItem.tintColor = UIColor.blue
+        barButtonItem.isEnabled = false
+        return barButtonItem
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Eligibility"
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        criteria = createCriteriaArray()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+    }
+    
+    func createCriteriaArray() -> [String] {
+        var tempCriteria: [String] = []
+        
+        let criterion1 = "You are 18 Years or Older"
+        let criterion2 = "You have had an Android or iPhone device for at least one year prior to enrolling in the study"
+        let criterion3 = "You have a Google account that is actively in use"
+        let criterion4 = "Spend time searching and browsing web on a weekly basis"
+        let criterion5 = "You spend time out of the house each day when feeling well (i.e. not largely home-bound"
+        tempCriteria.append(criterion1)
+        tempCriteria.append(criterion2)
+        tempCriteria.append(criterion3)
+        tempCriteria.append(criterion4)
+        tempCriteria.append(criterion5)
+        
+        return tempCriteria
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension InclusionExclusionViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return criteria.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let criterion = criteria[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EligibilityCell") as! EligibilityCell
+        
+        cell.criterion.text = criterion
+        print(cell.criterion.text)
+        return cell
+    }
+}
