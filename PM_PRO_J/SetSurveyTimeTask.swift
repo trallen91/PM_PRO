93a1b280532021b8c0b6d43373548e3b733b3cdf
Travis Allen <travis.william.allen@gmail.com>
2018-10-22 09:17:12 -0400

    set survey times front end now working

--- PM_PRO_J/SetSurveyTimeTask.swift
+++ PM_PRO_J/SetSurveyTimeTask.swift
@@ -16,31 +16,7 @@ public var SetSurveyTimeTask: ORKOrderedTask {
     instructionStep.text = "Please enter the preferred times you'd like to receive notifications for the surveys involved in this study"
     steps += [instructionStep]
     
-    //Get day of week and time for BIWEEKLY Standardized Survey
-//    let standardSurveyTime = ORKQuestionStep(identifier: "standardSurveyTime")
-//    standardSurveyTime.title = "Please enter a day of the week and a time"
-//    standardSurveyTime.text = "This survey is taken biweekly.  Select the ideal day and time to be notified to take this survey."
-//    standardSurveyTime.answerFormat = ORKLocationAnswerFormat()
-//    steps += [standardSurveyTime]
-    
-    //Get day of week and time for this WEEKLY patient-reported wellbeing survey
-    let wellBeingSurveyTime = ORKQuestionStep(identifier: "wellBeingSurveyTime")
-    wellBeingSurveyTime.title = "Please enter a day of the week"
-    wellBeingSurveyTime.text = "This survey is taken once weekly.  Select the ideal day and time to take this survey"
-    wellBeingSurveyTime.answerFormat = ORKLocationAnswerFormat()
-    steps += [wellBeingSurveyTime]
-    
-    //Get time for this DAILY patient-reported side effects survey
-    let sideEffectsSurveyTime = ORKQuestionStep(identifier: "sideEffectsSurveyTime")
-    sideEffectsSurveyTime.title = "This survey is taken daily.  Select the ideal day and time to take it"
-    sideEffectsSurveyTime.answerFormat = ORKLocationAnswerFormat()
-    steps += [sideEffectsSurveyTime]
-    
-    
-
-    
-
-//    let weekdayTitle = "Enter a day of the week"
+    //These answer formats will be used across all 3 steps
     let weekdayTextChoices = [
         ORKTextChoice(text: "Monday", value: "Monday" as NSString),
         ORKTextChoice(text: "Tuesday", value: "Tuesday" as NSString),
@@ -51,65 +27,41 @@ public var SetSurveyTimeTask: ORKOrderedTask {
         ORKTextChoice(text: "Sunday", value: "Sunday" as NSString)
         
     ]
-    
-    
     let weekdayAnswerFormat: ORKTextChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: .singleChoice, textChoices: weekdayTextChoices)
+    let timeAnswerFormat: ORKTimeOfDayAnswerFormat = ORKTimeOfDayAnswerFormat()
     
+    
+    //Standard Survey is a bi-weekly survey
     let standardSurveyTimeStep = ORKFormStep(identifier: "standardSurveyTime")
-    standardSurveyTimeStep.title = "Standardized Survey Notification Time"
-    standardSurveyTimeStep.text = "This survey is taken biweekly.  Select the ideal day and time to be notified to take this survey."
-    
+    standardSurveyTimeStep.title = "Standardized Survey"
+    standardSurveyTimeStep.text = "Please select the ideal day and time to take this BI-WEEKLY survey."
     
     let ssWeekday = ORKFormItem(identifier:"ssWeekday", text:"Enter day of week", answerFormat: weekdayAnswerFormat)
-    
-    standardSurveyTimeStep.formItems = [ssWeekday]
-    
-//    ssWeekday.answerFormat = weekdayAnswerFormat
-//    ssWeekday.
+    let ssTime = ORKFormItem(identifier:"ssTime", text:"Enter time of day", answerFormat: timeAnswerFormat)
+    standardSurveyTimeStep.formItems = [ssTime, ssWeekday]
     
      steps += [standardSurveyTimeStep]
     
-//    ORKFormStep *step =
-//        [[ORKFormStep alloc] initWithIdentifier:kFormIdentifier
-//            title:@"Form"
-//            text:@"Form groups multi-entry in one page"];
-//    NSMutableArray *items = [NSMutableArray new];
-//    ORKAnswerFormat *genderFormat =
-//        [ORKHealthKitCharacteristicTypeAnswerFormat
-//            answerFormatWithCharacteristicType:
-//            [HKCharacteristicType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierBiologicalSex]];
-//    [items addObject:
-//        [[ORKFormItem alloc] initWithIdentifier:kGenderItemIdentifier
-//        text:@"Gender"
-//        answerFormat:genderFormat];
-//
-//        // Include a section separator
-//        [items addObject:
-//        [[ORKFormItem alloc] initWithSectionTitle:@"Basic Information"]];
-//
-//        ORKAnswerFormat *bloodTypeFormat =
-//        [ORKHealthKitCharacteristicTypeAnswerFormat
-//        answerFormatWithCharacteristicType:
-//        [HKCharacteristicType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierBloodType]];
-//        [items addObject:
-//        [[ORKFormItem alloc] initWithIdentifier:kBloodTypeItemIdentifier
-//        text:@"Blood Type"
-//        answerFormat:bloodTypeFormat]];
-//
-//        ORKAnswerFormat *dateOfBirthFormat =
-//        [ORKHealthKitCharacteristicTypeAnswerFormat
-//        answerFormatWithCharacteristicType:
-//        [HKCharacteristicType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierDateOfBirth]];
-//        ORKFormItem *dateOfBirthItem =
-//        [[ORKFormItem alloc] initWithIdentifier:kDateOfBirthItemIdentifier
-//        text:@"DOB"
-//        answerFormat:dateOfBirthFormat]];
-//    dateOfBirthItem.placeholder = @"DOB";
-//    dateOfBirthItem.optional = YES;
-//    [items addObject:dateOfBirthItem];
-//
-//    // ... And so on, adding additional items
-//    step.formItems = items;
+    
+    //Get day of week and time for this WEEKLY patient-reported wellbeing survey
+    let wellBeingSurveyTime = ORKFormStep(identifier: "wellBeingSurveyTime")
+    wellBeingSurveyTime.title = "Well-Being Survey"
+    wellBeingSurveyTime.text = "Place select the ideal day and time to take this WEEKLY survey"
+    
+    let wbWeekday = ORKFormItem(identifier: "wbWeekday", text: "Enter day of week", answerFormat: weekdayAnswerFormat)
+    let wbTime = ORKFormItem(identifier: "wbTime", text:"Enter time of day", answerFormat: timeAnswerFormat)
+    
+    wellBeingSurveyTime.formItems = [wbTime, wbWeekday]
+    
+    steps += [wellBeingSurveyTime]
+    
+    //Get time for this DAILY patient-reported side effects survey
+    let sideEffectsSurveyTime = ORKQuestionStep(identifier: "sideEffectsSurveyTime")
+    sideEffectsSurveyTime.title = "Side Effects Survey"
+    sideEffectsSurveyTime.text = "Please enter the ideal time of day for you to take this DAILY survey"
+    sideEffectsSurveyTime.answerFormat = timeAnswerFormat
+    steps += [sideEffectsSurveyTime]
+    
     
     return ORKOrderedTask(identifier: "SetSurveyTimeTask", steps: steps)
 }
